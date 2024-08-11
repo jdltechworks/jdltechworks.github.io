b5100897c86477c44266c4e9acf82b779e158293
jo <jdltechworks@gmail.com>
2024-08-11 21:39:50 +0800

    Adding grinder

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-5px)' },
+          '50%': { transform: 'translateX(5px)' },
+          '75%': { transform: 'translateX(-5px)' },
+        },
+      },
+      animation: {
+        shake: 'shake 0.5s ease-in-out infinite',
+      },
+    }
   },
   plugins: [],
 }
